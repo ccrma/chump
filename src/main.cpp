610165ca4ab62cbc2f263a020d868eb2936ef21e
nshaheed <nicholasshaheed@gmail.com>
2024-04-18 13:36:24 -0700

    create ~/.chuck/lib/.chump dir on startup if it doesn't exist

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@
 
 #include "exec.h"
 #include "manager.h"
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 int main( int argc, const char ** argv ) {
   if (argc < 2) {
@@ -15,6 +18,18 @@ int main( int argc, const char ** argv ) {
   }
   auto [cmd, args] = parseArgs(argc, argv);
 
+
+  // create the necessary folders
+  // TODO: os-specific
+
+  // get home environment variable
+  const char* env_home = std::getenv("HOME");
+  fs::path path = fs::path(env_home);
+
+  path = path / ".chuck" / "lib" / ".chump";
+  fs::create_directories(path);
+
+  // Build manager and run command
   Manager* m = new Manager("../data/packages.json");
   execCommand(cmd, args, m);
 
