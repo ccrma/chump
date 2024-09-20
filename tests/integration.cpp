7ee51e3e1d678b9de1c0ff96b666f3b1ceb8c0d7
nshaheed <nicholasshaheed@gmail.com>
2024-09-19 19:11:01 -0700

    complete first integration test

--- tests/integration.cpp
+++ tests/integration.cpp
@@ -4,20 +4,20 @@
 #include <filesystem>
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("integration tests") {
-  // make a manager with fakes
+TEST_CASE("Integration Tests") {
 
   // reference: https://stackoverflow.com/questions/52912981/create-a-unique-temporary-directory
   fs::path installPath {fs::temp_directory_path() /= std::tmpnam(nullptr)};
   fs::create_directories(installPath);
-  
+
   std::string dataPath = "../data/packages.json";
 
   ChuckVersion ckVersion = ChuckVersion("1.5.2.0");
   ApiVersion langVersion = ApiVersion("9.1");
-  
+
   Manager* m = new Manager(dataPath, installPath, ckVersion, langVersion, "linux");
 
+  // install package
   std::cout << "the install dir: " << installPath << std::endl;
 
   std::vector<std::string> args = {"TestPackage"};
@@ -27,20 +27,20 @@ TEST_CASE("integration tests") {
   REQUIRE(fs::exists(installPath / "TestPackage" / "hello.ck"));
   REQUIRE(fs::exists(installPath / "TestPackage" / "version.json"));
 
-  // exec command update test
-
+  // Update Package
   ckVersion = ChuckVersion("1.5.2.6");
   langVersion = ApiVersion("10.1");
 
   m = new Manager(dataPath, installPath, ckVersion, langVersion, "linux");
 
-  args = {"TestPackage"};
-
   execCommand("update", args, m);
 
   REQUIRE(fs::exists(installPath / "TestPackage"));
   REQUIRE(fs::exists(installPath / "TestPackage" / "hello.ck"));
-  REQUIRE(fs::exists(installPath / "TestPackage" / "version.json")); 
+  CHECK(fs::exists(installPath / "TestPackage" / "version.json"));
 
-  // exec command uninstall test
+  // Uninstall Package
+  execCommand("uninstall", args, m);
+
+  REQUIRE_FALSE(fs::exists(installPath / "TestPackage"));
 }
