66b3c0f0e661c93986f61d2211031cc46d5dbcb2
nshaheed <nicholasshaheed@gmail.com>
2024-10-05 13:49:57 -0700

    move cli args processing to CLI11

--- tests/exec_test.cpp
+++ tests/exec_test.cpp
@@ -1,17 +1,2 @@
 #include "exec.h"
 #include <catch2/catch_test_macros.hpp>
-
-TEST_CASE("parseArgs", "[parseargs]") {
-
-  SECTION("Multiple args") {
-    int argc = 4;
-    const char * argv[] = {"programname", "a", "b", "c"};
-
-    auto [cmd, args] = parseArgs(argc, argv);
-
-    std::vector<std::string> want({"b", "c"});
-
-    REQUIRE(cmd == "a");
-    REQUIRE(args == want);
-  }
-}
