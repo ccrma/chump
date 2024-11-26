051b94010e51aedd9ac225052cfeed05feb847f7
Ge Wang <ge@ccrma.stanford.edu>
2024-11-25 16:20:17 -0800

    reformat main.cpp to 4-spaces

--- chump-cli/main.cpp
+++ chump-cli/main.cpp
@@ -1,208 +1,209 @@
-// ChuMP (ChucK Manager of Programs) CLI main file.
+//-----------------------------------------------------------------------------
+// ChuMP (ChucK Manager of Programs) CLI main file
+// author: Nick Shaheed
+//   date: Fall 2024
+//-----------------------------------------------------------------------------
+#include "chuck_version.h"
+#include "exec.h"
+#include "manager.h"
+#include "util.h"
+#include <filesystem>
+
+#include "CLI/CLI.hpp"
+
+namespace fs = std::filesystem;
+using std::string;
+
 
 //-----------------------------------------------------------------------------
 // name: main()
 // desc: command line chuck entry point
 //-----------------------------------------------------------------------------
+int main( int argc, const char ** argv )
+{
+    // check if we output to TTY (teletype interactive terminal)
+    // if not disable color teriminal to avoid outputing
+    // ANSI escape codes to the output stream, which would
+    // show up in | and >
+    t_CKBOOL colorTerminal = ck_isatty();
+    // set the color terminal global setting
+    TC::globalDisableOverride( !colorTerminal );
 
-#include "chuck_version.h"
-#include "exec.h"
-#include "manager.h"
-#include "util.h"
+    /****************************************************************
+     * CLI Args Setup
+     ****************************************************************/
+    CLI::App app;
 
-#include "CLI/CLI.hpp"
+    string usage = printLogo() + "\n" + "usage: chump [options] [command]";
+    app.usage(usage);
 
-#include <filesystem>
-
-namespace fs = std::filesystem;
-using std::string;
-
-int main( int argc, const char ** argv ) {
-  // check if we output to TTY (teletype interactive terminal)
-  // if not disable color teriminal to avoid outputing
-  // ANSI escape codes to the output stream, which would
-  // show up in | and >
-  t_CKBOOL colorTerminal = ck_isatty();
-  // set the color terminal global setting
-  TC::globalDisableOverride( !colorTerminal );
-
-  /****************************************************************
-   * CLI Args Setup
-   ****************************************************************/
-  CLI::App app;
-
-  string usage = printLogo() + "\n" + "Usage: chump [OPTIONS] [SUBCOMMAND]";
-  app.usage(usage);
-
-  CLI::App* info = app.add_subcommand("info", "display information about <package>");
-  string info_package_name;
-  info
+    CLI::App* info = app.add_subcommand("info", "display information about <package>");
+    string info_package_name;
+    info
     ->add_option("package", info_package_name, "package to print info about")
     ->option_text("")
     ->required();
 
-  CLI::App* list = app.add_subcommand("list", "list available packages");
-  bool installed_flag = false;
-  list->add_flag("-i, --installed", installed_flag, "list only installed packages");
+    CLI::App* list = app.add_subcommand("list", "list available packages");
+    bool installed_flag = false;
+    list->add_flag("-i, --installed", installed_flag, "list only installed packages");
 
-
-  CLI::App* install = app.add_subcommand("install", "download and install <package>");
-  string install_package_name;
-  install
+    CLI::App* install = app.add_subcommand("install", "download and install <package>");
+    string install_package_name;
+    install
     ->add_option("package", install_package_name, "package to install (case sensitive)")
     ->option_text("(=<version>)")
     ->required();
 
-  CLI::App* uninstall = app.add_subcommand("uninstall", "uninstall <package>");
-  string uninstall_package_name;
-  uninstall
+    CLI::App* uninstall = app.add_subcommand("uninstall", "uninstall <package>");
+    string uninstall_package_name;
+    uninstall
     ->add_option("package", uninstall_package_name, "package to uninstall (case sensitive)")
     ->required();
 
-  CLI::App* update = app.add_subcommand("update", "update <package> to latest compatible version");
-  string update_package_name;
-  auto pkg_opt = update
+    CLI::App* update = app.add_subcommand("update", "update <package> to latest compatible version");
+    string update_package_name;
+    auto pkg_opt = update
     ->add_option("package", update_package_name, "package to update (case sensitive)");
     // ->required();
-  bool update_package_list = false;
-  auto update_list_flag = update->add_flag("-u, --update-list", update_package_list, "update list of available packages");
+    bool update_package_list = false;
+    auto update_list_flag = update->add_flag("-u, --update-list", update_package_list, "update list of available packages");
 
-  update_list_flag->excludes(pkg_opt);
+    update_list_flag->excludes(pkg_opt);
 
-  // This is where the fun is
-  CLI::App* cereal = app.add_subcommand("cereal", "a fruity breakfast treat");
-  CLI::App* river = app.add_subcommand("river", "flowing & ebbing");
-  CLI::App* bedtime = app.add_subcommand("bedtime", "zzzzz....");
-  CLI::App* dim = app.add_subcommand("dim", "what's there?");
+    // This is where the fun is
+    CLI::App* cereal = app.add_subcommand("cereal", "a fruity breakfast treat");
+    CLI::App* river = app.add_subcommand("river", "flowing & ebbing");
+    CLI::App* bedtime = app.add_subcommand("bedtime", "zzzzz....");
+    CLI::App* dim = app.add_subcommand("dim", "what's there?");
 
-  app.require_subcommand(1); // only want a single command here
+    app.require_subcommand(1); // only want a single command here
 
-  CLI11_PARSE(app, argc, argv);
+    CLI11_PARSE(app, argc, argv);
 
-  /****************************************************************
-   * Chump setup
-   ****************************************************************/
+    /****************************************************************
+     * Chump setup
+     ****************************************************************/
 
-  fs::path path = chumpDir();
+    fs::path path = chumpDir();
 
-  fs::create_directories(path);
+    fs::create_directories(path);
 
-  Manager* manager;
+    Manager* manager;
 
-  // Build manager and run command
-  fs::path pkg_path = chumpDir() / "manifest.json";
+    // Build manager and run command
+    fs::path pkg_path = chumpDir() / "manifest.json";
 
-  std::string manifest_url = "https://ccrma.stanford.edu/~nshaheed/chump/manifest.json";
+    std::string manifest_url = "https://ccrma.stanford.edu/~nshaheed/chump/manifest.json";
 
-  // if the manifesto isn't loading properly, only allow `chump update -u`.
-  // This is an escape hatch, because failing to parse manifest.json will
-  // result in an exception and the program won't continue.
-  if (!validate_manifest(pkg_path)) {
-    if (!update_package_list) return -1; // exit if we're not updating the manifest
+    // if the manifesto isn't loading properly, only allow `chump update -u`.
+    // This is an escape hatch, because failing to parse manifest.json will
+    // result in an exception and the program won't continue.
+    if (!validate_manifest(pkg_path)) {
+        if (!update_package_list) return -1; // exit if we're not updating the manifest
 
-    manager = new Manager("", path, ChuckVersion::makeSystemVersion(),
-                          ApiVersion::makeSystemVersion(), whichOS(), manifest_url, true);
+        manager = new Manager("", path, ChuckVersion::makeSystemVersion(),
+                              ApiVersion::makeSystemVersion(), whichOS(), manifest_url, true);
 
-    manager->update_manifest();
+        manager->update_manifest();
 
-    return 1;
-  }
-
-
-  try {
-    manager = new Manager(pkg_path.string(), path, ChuckVersion::makeSystemVersion(),
-                          ApiVersion::makeSystemVersion(), whichOS(), manifest_url, true);
-  } catch (const std::exception &e) {
-    std::cerr << e.what() << std::endl;
-    return 1;
-  }
-
-  if (app.got_subcommand(info)) {
-    optional<Package> pkg = manager->getPackage(info_package_name);
-
-    if (!pkg) {
-      std::cerr << "Unable to find package " << info_package_name << std::endl;
-      return 1;
+        return 1;
     }
 
-    std::cout << pkg.value() << std::endl;
-  } else if (app.got_subcommand(list)) {
-    printPackages(manager, installed_flag);
-  } else if (app.got_subcommand(install)) {
-    manager->install(install_package_name);
-  } else if (app.got_subcommand(uninstall)) {
-    manager->uninstall(uninstall_package_name);
-  } else if (app.got_subcommand(update)) {
-    if (update_package_list) {
-      manager->update_manifest();
-    } else {
-      manager->update(update_package_name);
+    try {
+        manager = new Manager(pkg_path.string(), path, ChuckVersion::makeSystemVersion(),
+                              ApiVersion::makeSystemVersion(), whichOS(), manifest_url, true);
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
     }
-  } else if (app.got_subcommand(cereal)) {
-    int counter = 0;
-    while (true) {
 
-      std::string logo = jumbleLogo(counter);
-      std::cout << logo;
+    if (app.got_subcommand(info)) {
+        optional<Package> pkg = manager->getPackage(info_package_name);
 
-      auto num_lines = std::count( logo.begin(), logo.end(), '\n' );
+        if (!pkg) {
+            std::cerr << "unable to find package " << info_package_name << std::endl;
+            return 1;
+        }
 
-      for (int i = 0; i < num_lines; i++) {
-        std::cout << "\x1b[F"; // Move the cursor up one line
-      }
-      counter++;
-      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::cout << pkg.value() << std::endl;
+    } else if (app.got_subcommand(list)) {
+        printPackages(manager, installed_flag);
+    } else if (app.got_subcommand(install)) {
+        manager->install(install_package_name);
+    } else if (app.got_subcommand(uninstall)) {
+        manager->uninstall(uninstall_package_name);
+    } else if (app.got_subcommand(update)) {
+        if (update_package_list) {
+            manager->update_manifest();
+        } else {
+            manager->update(update_package_name);
+        }
+    } else if (app.got_subcommand(cereal)) {
+        int counter = 0;
+        while (true) {
+
+            std::string logo = jumbleLogo(counter);
+            std::cout << logo;
+
+            auto num_lines = std::count( logo.begin(), logo.end(), '\n' );
+
+            for (int i = 0; i < num_lines; i++) {
+                std::cout << "\x1b[F"; // Move the cursor up one line
+            }
+            counter++;
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    } else if (app.got_subcommand(river)) {
+        int counter = 0;
+        while (true) {
+
+            std::string logo = riverLogo(counter);
+            std::cout << logo;
+
+            auto num_lines = std::count( logo.begin(), logo.end(), '\n' );
+
+            for (int i = 0; i < num_lines; i++) {
+                std::cout << "\x1b[F"; // Move the cursor up one line
+            }
+            counter++;
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    } else if (app.got_subcommand(bedtime)) {
+        int counter = 0;
+        while (true) {
+
+            std::string logo = bedtimeLogo(counter);
+
+            int width=0, height=0;
+            get_terminal_size(width, height);
+
+            int i = 0;
+            while (i < logo.length()) {
+                std::cout << logo.substr(i, width);
+                i += width;
+                std::this_thread::sleep_for(std::chrono::milliseconds(2));
+            }
+
+            counter++;
+        }
+    } else if (app.got_subcommand(dim)) {
+        clear();
+        int counter = 0;
+        while (true) {
+
+            std::string logo = dimLogo(counter);
+            std::cout << logo;
+
+            auto num_lines = std::count( logo.begin(), logo.end(), '\n' );
+
+            for (int i = 0; i < num_lines; i++) {
+                std::cout << "\x1b[F"; // Move the cursor up one line
+            }
+            counter++;
+            std::this_thread::sleep_for(std::chrono::milliseconds(120));
+        }
     }
-  } else if (app.got_subcommand(river)) {
-    int counter = 0;
-    while (true) {
 
-      std::string logo = riverLogo(counter);
-      std::cout << logo;
-
-      auto num_lines = std::count( logo.begin(), logo.end(), '\n' );
-
-      for (int i = 0; i < num_lines; i++) {
-        std::cout << "\x1b[F"; // Move the cursor up one line
-      }
-      counter++;
-      std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
-  } else if (app.got_subcommand(bedtime)) {
-    int counter = 0;
-    while (true) {
-
-      std::string logo = bedtimeLogo(counter);
-
-      int width=0, height=0;
-      get_terminal_size(width, height);
-
-      int i = 0;
-      while (i < logo.length()) {
-        std::cout << logo.substr(i, width);
-        i += width;
-        std::this_thread::sleep_for(std::chrono::milliseconds(2));
-      }
-
-      counter++;
-    }
-  } else if (app.got_subcommand(dim)) {
-    clear();
-    int counter = 0;
-    while (true) {
-
-      std::string logo = dimLogo(counter);
-      std::cout << logo;
-
-      auto num_lines = std::count( logo.begin(), logo.end(), '\n' );
-
-      for (int i = 0; i < num_lines; i++) {
-        std::cout << "\x1b[F"; // Move the cursor up one line
-      }
-      counter++;
-      std::this_thread::sleep_for(std::chrono::milliseconds(120));
-    }
-  }
-
-  return 0;
+    return 0;
 }
